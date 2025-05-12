4a52b568f63e4ab147225b5fc7dd3f45a5d7c2e6
Charles Merkel <155692216+CharlesMerkel@users.noreply.github.com>
2025-05-12 16:45:31 -0700

    naming convention comments

--- IceMan/IceMan/Actor.h
+++ IceMan/IceMan/Actor.h
@@ -2,6 +2,14 @@
 #define ACTOR_H_
 
 #include "GraphObject.h"
+//variables
+// private : _nameOfThing
+// public : nameOfThing
+
+// Functions :
+// private : _NameOfThing()
+// public : NameOfThing()
+
 
 // Students:  Add code to this file, Actor.cpp, StudentWorld.h, and StudentWorld.cpp
 //comment added
