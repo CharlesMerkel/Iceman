c6cd9425ba7efccbcd89d8018e5f6f5d90bacdf8
Josch Angco <schoolaccjangco@gmail.com>
2025-05-14 18:44:01 -0700

    Just added other classes

--- IceMan/IceMan/Actor.h
+++ IceMan/IceMan/Actor.h
@@ -10,7 +10,68 @@
 // private : _NameOfThing()
 // public : NameOfThing()
 
+class Ice : public Actor
+{
+public:
 
+private:
+};
+
+class Boulder : public Actor
+{
+public:
+
+private:
+};
+
+class Squirt : public Actor
+{
+public:
+
+private:
+};
+
+class Oil : public Actor
+{
+public:
+
+private:
+};
+
+class Gold : public Actor
+{
+public:
+
+private:
+};
+
+class Sonar : public Actor
+{
+public:
+
+private:
+};
+
+class WaterPool : public Actor
+{
+public:
+
+private:
+};
+
+class Protesters : public Actor
+{
+public:
+
+private:
+};
+
+class HardcoreProtesters : public Actor
+{
+public:
+
+private:
+};
 // Students:  Add code to this file, Actor.cpp, StudentWorld.h, and StudentWorld.cpp
 //comment added
 #endif // ACTOR_H_
